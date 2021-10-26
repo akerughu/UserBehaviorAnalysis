c8a35b83d8c11407512cd2cf83dd786538772c0c
akerughu <1029756481@qq.com>
2021-10-27 00:04:33 +0800

    实时热门商品 排序输出TopN

--- HotItemsAnalysis/src/main/java/com/atguigu/hotitems_analysis/HotItems.java
+++ HotItemsAnalysis/src/main/java/com/atguigu/hotitems_analysis/HotItems.java
@@ -2,16 +2,24 @@ package com.atguigu.hotitems_analysis;
 
 import com.atguigu.hotitems_analysis.beans.ItemViewCount;
 import com.atguigu.hotitems_analysis.beans.UserBehavior;
+import org.apache.commons.compress.utils.Lists;
 import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.api.common.state.ListState;
+import org.apache.flink.api.common.state.ListStateDescriptor;
 import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
 import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
 
 public class HotItems {
     public static void main(String[] args) throws Exception {
@@ -41,6 +49,12 @@ public class HotItems {
                 .keyBy("itemId")   // 按商品id分组
                 .timeWindow(Time.hours(1), Time.minutes(5))   // 开滑动窗口
                 .aggregate(new ItemCountAgg(), new WindowItemCountResult());
+
+        // 5、收集同一窗口的所有商品count数据，排序输出topN
+        DataStream<String> resultStream = windowAggStream.keyBy("windowEnd")
+                .process(new TopNHotItems(5));  // 用自定义处理函数排序取前5
+
+        resultStream.print();
 
         env.execute("hot items analysis");
     }
@@ -76,6 +90,58 @@ public class HotItems {
             Long windowEnd = timeWindow.getEnd();
             Long count = iterable.iterator().next();
             collector.collect(new ItemViewCount(itemId, windowEnd, count));
+        }
+    }
+
+    // 实现自定义KeyedProcessFunction
+    public static class TopNHotItems extends KeyedProcessFunction<Tuple, ItemViewCount, String> {
+        // 定义属性TOPN的大小
+        private Integer topSize;
+
+        public TopNHotItems(Integer topSize) {
+            this.topSize = topSize;
+        }
+
+        // 定义列表状态，保存当前窗口内所有输出的ItemViewCount
+        ListState<ItemViewCount> itemViewCountListState;
+
+        @Override
+        public void open(Configuration parameters) throws Exception {
+            itemViewCountListState = getRuntimeContext().getListState(new ListStateDescriptor<ItemViewCount>("item-view-count-list", ItemViewCount.class));
+        }
+
+        @Override
+        public void processElement(ItemViewCount itemViewCount, Context context, Collector<String> collector) throws Exception {
+            // 每来一条数据，存入list中，并注册定时器
+            itemViewCountListState.add(itemViewCount);
+            context.timerService().registerEventTimeTimer( itemViewCount.getWindowEnd() + 1 );
+        }
+
+        @Override
+        public void onTimer(long timestamp, OnTimerContext ctx, Collector<String> out) throws Exception {
+            // 定时器触发，当前已收集到所有数据，排序输出TOPN
+            ArrayList<ItemViewCount> itemViewCounts = Lists.newArrayList(itemViewCountListState.get().iterator());
+            itemViewCounts.sort((o1, o2) -> o2.getCount().intValue() - o1.getCount().intValue());
+
+            // 将排名信息格式化成String，方便打印输出
+            StringBuilder resultBuilder = new StringBuilder();
+            resultBuilder.append("=====================");
+            resultBuilder.append("窗口结束时间：").append(new Timestamp(timestamp - 1)).append("\n");
+
+            // 遍历列表，取TopN输出
+            for (int i=0; i<Math.min(topSize, itemViewCounts.size()); i++) {
+                ItemViewCount currentItemViewCount = itemViewCounts.get(i);
+                resultBuilder.append("NO ").append(i+1).append(": ")
+                        .append("商品ID=").append(currentItemViewCount.getItemId())
+                        .append("热门度=").append(currentItemViewCount.getCount())
+                        .append("\n");
+            }
+            resultBuilder.append("=====================\n\n");
+
+            // 控制输出频率
+            Thread.sleep(1000L);
+
+            out.collect(resultBuilder.toString());
         }
     }
 }
